4d5037a375f002bebdefc8348718fd928d1af0b2
Humphrey Hu <humhu@cmu.edu>
2015-08-21 17:19:13 -0400

    Added input parsing and output logging to threaded calibration.

--- app/ThreadedCalibration.cpp
+++ app/ThreadedCalibration.cpp
@@ -3,6 +3,8 @@
 #include <boost/foreach.hpp>
 #include <boost/bind.hpp>
 #include <Eigen/Dense>
+#include <fstream>
+#include <unistd.h>
 
 #include "calotypes/CrossValidation.hpp"
 #include "calotypes/CameraCalibration.h"
@@ -12,17 +14,55 @@
 
 using namespace calotypes;
 
+void PrintHelp()
+{
+	std::cout << "threaded_calibration [-d datalogPath] [-o outputPath] [-k num folds(4)]" << std::endl;
+}
+
 int main( int argc, char** argv )
 {
 	
-	if( argc < 2 )
+	std::string configPath, outputPath;
+	unsigned int numFolds = 4;
+	char c;
+	while( (c = getopt( argc, argv, "hd:o:k:")) != -1 )
 	{
-		std::cerr << "Please specify config file path." << std::endl;
-		return -1;
+		switch( c )
+		{
+			case 'h':
+				PrintHelp();
+				return 0;
+			case 'd':
+				configPath.assign( optarg );
+				break;
+			case 'o':
+				outputPath.assign( optarg );
+				break;
+			case 'k':
+				numFolds = strtol( optarg, NULL, 10 );
+			case '?':
+                if (isprint (optopt)) 
+				{
+                    std::cerr << "Unknown option -" << optopt << std::endl;
+                } 
+                else 
+				{
+                    std::cerr << "Unknown option character " << optopt << std::endl;
+                }
+                return -1;
+            default:
+				std::cerr << "Unrecognized option." << std::endl;
+                return -1;
+		}
 	}
 	
-	std::string configPath( argv[1] );
 	CalibrationLogReader reader( configPath );
+	std::ofstream outputLog( outputPath );
+	if( !outputLog.is_open() )
+	{
+		std::cerr << "Could not open output log at " << outputPath << std::endl;
+		return -1;
+	}
 	
 	CameraTrainingData datum;
 	std::vector<CameraTrainingData> data;
@@ -50,8 +90,6 @@ int main( int argc, char** argv )
 	CameraCV::TestFunc tester = boost::bind( &TestCameraModel, _1, _2 );
 	CameraCV crossValidation( trainer, tester, subset, 4 );
 	
-	unsigned int numFolds = 4;
-	
 	WorkerPool pool( 4 );
 	pool.StartWorkers();
 	
@@ -65,12 +103,18 @@ int main( int argc, char** argv )
 	
 	std::vector< CameraCV::ValidationResult > results = crossValidation.GetResults();
 	
+	outputLog << "cross-validation results for " << configPath << std::endl;
+	outputLog << "k = " << numFolds << std::endl;
+	outputLog << "[fold_index] [train_error] [test_error]" << std::endl;
+	
 	std::cout << "Errors: " << std::endl;
 	for( unsigned int i = 0; i < numFolds; i++ )
 	{
 		std::cout << "\tFold " << i << " test/train: " << results[i].testError
 				  << " " << results[i].trainingError << std::endl;
 		std::cout << "\tModel " << results[i].model << std::endl;
+		
+		outputLog << i << " " << results[i].trainingError << " " << results[i].testError << std::endl;
 	}
 	
 	return 0;
